80d5b7103d0aa4f7f8ddcf188ab2cd76dda4bedc
ots <josemmateosd@gmail.com>
2009-07-29 09:05:49 +0200

    update

---
+++ mpiVersion/quicksort.h
@@ -0,0 +1,23 @@
+/******************************************************************************
+* FILE: quicksort.h
+* jmateos@uma.es
+* LAST REVISED: 13/04/09
+******************************************************************************/
+
+#include <stdio.h>
+
+
+
+#define SMALLSIZE       10            // not less than 3
+
+
+struct stack {                              // stack element.
+        int a,b;
+} * s;
+
+
+
+
+void interchange(double *x, double *y);
+void interchangeIndex(int * dIndex, int index1,int index2);
+  
